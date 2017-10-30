7f30ea7c27440e22222c80775df7103abacb7ad2
ajlim2 <aaronlim221@gmail.com>
2017-10-30 12:59:41 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 package edu.coo.cs.cs141.final_prog_assignment;
-//test 
+
 public class Main {
 
 	public static void main(String[] args) {
 		UserInterface ui = new UserInterface(new gameEngine());
 		ui.openMenu();
+
 	}
 
 }
