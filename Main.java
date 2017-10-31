a119e71b48830c0b507bdf5034bff45d7f6329ee
jonjxtsu <jcamarillo88@gmail.com>
2017-10-31 01:05:00 -0700

    Capitalized classes, lowercased objects

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
+/*
+ * 
+ */
 package edu.coo.cs.cs141.final_prog_assignment;
 
 public class Main {
 // hello testing ;lkasjdf;lkj
 	public static void main(String[] args) {
-		UserInterface ui = new UserInterface(new gameEngine());
+		UserInterface ui = new UserInterface(new GameEngine());
 		ui.openMenu();
 //second test
 	}
