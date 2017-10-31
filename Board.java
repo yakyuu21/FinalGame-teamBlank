457fac9074d3b6c2d5b8ac6de99fe6a2fbc81e01
jonjxtsu <jcamarillo88@gmail.com>
2017-10-31 10:46:45 -0700

    briefcase added

--- Board.java
+++ Board.java
@@ -1,20 +1,30 @@
+/*
+ * 
+ */
+
 package edu.coo.cs.cs141.final_prog_assignment;
+import java.util.Random;
 
 public class Board {
 	final int horizontal = 9;
 	final int vertical = 9;
 	String[][] board = new String [horizontal][vertical];
+	Random random = new Random();
+
 	private int depth;
+
+	int[] briefcaseX = {1,4,7};
+	int[] briefcaseY = {1,4,7};
 	
 	Board() {
 		createBoard();
 	}
-	
+
 	public void createBoard() {
-		
+
 		for(int x=0;x<horizontal;x++) {
 			for(int y=0;y<vertical;y++) {
-				if((y==2 || y==4 || y==6) && (x==2 || x==4 || x==6)) {
+				if((y==1 || y==4 || y==7) && (x==1 || x==4 || x==7)) {
 					board[x][y] = "?";
 				}
 				else 
@@ -22,12 +32,12 @@ public class Board {
 			}
 		}
 	}
-	
+
 	public void overWrite(int x, int y) {
 		createBoard();
 		board[x][y] = "P";
 	}
-	
+
 	public void displayBoard() {
 		for(int x=0;x<horizontal;x++) {
 			System.out.println("");
@@ -35,6 +45,16 @@ public class Board {
 				System.out.print(board[x][y] + " ");
 			}
 		}
-	System.out.println("\n");		
+
+		System.out.println("\n");		
 	}
+	
+
+	
+	public void spawnBriefcase()
+	{
+		board[briefcaseX[random.nextInt(2)]][briefcaseY[random.nextInt(2)]] = "B";
+		displayBoard();//Don't have to show.
+	}
+	
 }
