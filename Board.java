0b4a8bb61e2cb655f52173980ddd34ad66c64bb4
Sjmarrero <33099245+Sjmarrero@users.noreply.github.com>
2017-10-31 10:57:10 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -10,6 +10,9 @@ public class Board {
 	final int vertical = 9;
 	String[][] board = new String [horizontal][vertical];
 	Random random = new Random();
+	private PowerUp radar = null;
+	private PowerUp invincible = null;
+	private PowerUp ammo = null;
 
 	private int depth;
 
@@ -31,6 +34,42 @@ public class Board {
 					board[x][y] = "O";
 			}
 		}
+		createPowerUp();
+	}
+	
+	private void createPowerUp() {
+		int x, y;
+		while(radar == null) {
+			x = getRandom();
+			y = getRandom();
+			if(Board[x][y] != "O") {}
+			else {
+				radar = new PowerUp("radar", x, y);
+				Board[x][y] = "r";
+			}
+		}
+		while(invincible == null) {
+			x = getRandom();
+			y = getRandom();
+			if(Board[x][y] != "O") {}
+			else {
+				invincible = new PowerUp("invincible", x, y);
+				Board[x][y] = "i";
+			}
+		}
+		while(ammo == null) {
+			x = getRandom();
+			y = getRandom();
+			if(Board[x][y] != "O") {}
+			else {
+				ammo = new PowerUp("ammo", x, y);
+				Board[x][y] = "a";
+			}
+		}	
+	}
+	
+	public int getRandom(){
+		return random.nextInt(9);
 	}
 
 	public void overWrite(int x, int y) {
