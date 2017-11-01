a80b8fcdcd6366079457c26ba91bee8bb4d2f02b
Sjmarrero <33099245+Sjmarrero@users.noreply.github.com>
2017-10-31 17:45:27 -0700

    Update GameEngine.java

--- GameEngine.java
+++ GameEngine.java
@@ -1,16 +1,22 @@
 package edu.cpp.cs.cs141.final_prog_assignment;
 
 public class GameEngine {
-	private int playerX, playerY;
+	
 	private boolean wall;
+	Random random = new Random();
+	
+	private Characters player;
+	private Board board;
+	private PowerUp radar = null;
+	private PowerUp invincible = null;
+	private PowerUp ammo = null;
+	
 	public void play() {
 		wall = false;
 		while(!wall) {
-			youTurn();
+			yourTurn();
 		}
 	}
-	
-	
 	
 	public void yourTurn() {
 		
@@ -20,8 +26,48 @@ public class GameEngine {
 		return true;
 	}
 	
+	public void createBoard() {
+		player = new Characters(0,8);
+		board = new Board();
+		createPowerUp();
+		
+	}
 	
-
+	private void createPowerUp() {
+		int x, y;
+		while(radar == null) {
+			x = getRandom();
+			y = getRandom();
+			if(board.at(x, y) != "O") {}
+			else {
+				radar = new PowerUp("radar", x, y);
+				board.overWrite(x,y,"r");
+			}
+		}
+		while(invincible == null) {
+			x = getRandom();
+			y = getRandom();
+			if(board.at(x,y) != "O") {}
+			else {
+				invincible = new PowerUp("invincible", x, y);
+				board.overWrite(x,y,"i");;
+			}
+		}
+		while(ammo == null) {
+			x = getRandom();
+			y = getRandom();
+			if(board.at(x,y) != "O") {}
+			else {
+				ammo = new PowerUp("ammo", x, y);
+				board.overWrite(x,y,"a");
+			}
+		}	
+	}
+	
+	public int getRandom(){
+		return random.nextInt(9);
+	}
+	
 }
 
 
